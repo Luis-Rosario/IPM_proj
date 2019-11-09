27198a5d6fda633c0ebd0ce9f5eb5bc09262899f
Luis-Rosario <meltedcorn@gmail.com>
2019-11-09 16:50:25 +0000

    update v999

---
+++ src/app/features/content/browse-page/browse-results/browse-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'browse-results',
+  templateUrl: './browse-results.component.html',
+  styleUrls: ['./browse-results.component.scss']
+})
+export class BrowseResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/content/content.module.ts
+++ src/app/features/content/content.module.ts
@@ -5,12 +5,14 @@ import { ContentRoutingModule } from './content-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
 import { BrowsePageComponent } from './browse-page/browse-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { BrowseResultsComponent } from './browse-page/browse-results/browse-results.component';
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    BrowsePageComponent
+    BrowsePageComponent,
+    BrowseResultsComponent
   ],
   imports: [
     CommonModule,
