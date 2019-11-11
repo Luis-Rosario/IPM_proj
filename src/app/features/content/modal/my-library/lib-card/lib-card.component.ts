821b2f79c38b29721af8e423abf9cee9741e58d1
Francisco Silva <fsilva@imaginarycloud.com>
2019-11-11 23:42:21 +0000

    added my library and generaljs hack

--- src/app/features/content/modal/my-library/lib-card/lib-card.component.ts
+++ src/app/features/content/modal/my-library/lib-card/lib-card.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input } from "@angular/core";
+import * as $ from "jquery";
 @Component({
-  selector: 'lib-card',
-  templateUrl: './lib-card.component.html',
-  styleUrls: ['./lib-card.component.scss']
+  selector: "lib-card",
+  templateUrl: "./lib-card.component.html",
+  styleUrls: ["./lib-card.component.scss"]
 })
 export class LibCardComponent implements OnInit {
   @Input() gameName: String;
-  @Input() gameInfo: any
-  
-  constructor() { }
+  @Input() gameInfo: any;
+
+  constructor() {}
 
   ngOnInit() {
+    $(".pill").click(function() {
+      $(this).toggleClass("active");
+    });
   }
-
 }
